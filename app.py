c93ae67b1423100e98657b4bc3f3460b516e47f4
Syed Maaz Masood <syed.maaz.masood@gmail.com>
2025-11-10 13:01:53 -0500

    UPDATE- Security bypass fix

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if uploaded_file:
                 llm,
                 df,
                 agent_type="openai-tools",
-                verbose=True # Set to True to see the agent's "thinking" in your terminal,
+                verbose=True, # Set to True to see the agent's "thinking" in your terminal
                 allow_dangerous_code=True
             )
 
